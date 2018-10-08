231a190c7332c7397850d460d90033e446ecf941
Nikita Romashenkov <39829500+romashenkov@users.noreply.github.com>
2018-10-08 12:57:46 +0300

    Updates of dice behavior
    First steps of DOM access and manipulation
    Random number creating for dice is added

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1; /* or 1 */
+
+dice = Math.floor(Math.random() * 6) + 1; /*random number from 1 to 6: Math.random gives a random num from 0 to 1, Math.floor extracts integer part*/
+
+/*the object that gives us access to the DOM is the document object*/
+document.querySelector('#current-' + activePlayer).textContent = dice; /*querySelector finds an element in DOM by an id; textContent changes the plain TEXT of the element*/
+
+/*document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; // innerHTML changes the HTML element*/
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none' /*we change the property in CSS class selector in order to hide the random dice in the begining*/
