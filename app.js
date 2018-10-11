96d8c3ce20a9b4e2ffa820e5fe995aaaaa7b8249
Nikita Romashenkov <39829500+romashenkov@users.noreply.github.com>
2018-10-11 23:51:18 +0300

    Changing the winning score
    20 is changed to 100

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		//update the UI
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 		// check if player won the game
-		if (scores[activePlayer] >= 20) {
+		if (scores[activePlayer] >= 100) {
 			document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
